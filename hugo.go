86f155490468d5fb7bdcebdce0c9be22c20fd61a
Henrique Dias <hacdias@gmail.com>
2015-09-12 11:33:39 +0100

    hugo

--- hugo.go
+++ hugo.go
@@ -3,11 +3,17 @@ package hugo
 import (
 	"net/http"
 
+	"github.com/spf13/hugo/commands"
+
 	"github.com/mholt/caddy/config/setup"
 	"github.com/mholt/caddy/middleware"
 )
 
 func Setup(c *setup.Controller) (middleware.Middleware, error) {
+	for c.Next() {
+		commands.Execute()
+	}
+
 	return func(next middleware.Handler) middleware.Handler {
 		return &handler{}
 	}, nil
@@ -16,6 +22,6 @@ func Setup(c *setup.Controller) (middleware.Middleware, error) {
 type handler struct{}
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
-	w.Write([]byte("Hello, I'm a caddy middleware"))
+	http.ServeFile(w, r, "public"+r.URL.Path)
 	return 200, nil
 }
