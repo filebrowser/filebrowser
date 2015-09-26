e7528ba7c9c8e5101364994e7c6d24b7f6e72481
Henrique Dias <hacdias@gmail.com>
2015-09-26 13:03:02 +0100

    add some tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -37,3 +37,73 @@ func TestCanBeEdited(t *testing.T) {
 		}
 	}
 }
+
+func TestCopyFile(t *testing.T) {
+	err := CopyFile("test_data/file_to_copy.txt", "test_data/copied_file.txt")
+
+	if err != nil {
+		t.Error("Can't copy the file.")
+	}
+}
+
+type testDefinedData struct {
+	f1 string
+	f2 bool
+	f3 int
+	f4 func()
+}
+
+type testDefined struct {
+	data   interface{}
+	field  string
+	result bool
+}
+
+var testDefinedCases = []testDefined{
+	{testDefinedData{}, "f1", true},
+	{testDefinedData{}, "f2", true},
+	{testDefinedData{}, "f3", true},
+	{testDefinedData{}, "f4", true},
+	{testDefinedData{}, "f5", false},
+	{[]string{}, "", false},
+	{map[string]int{"oi": 4}, "", false},
+	{"asa", "", false},
+	{"int", "", false},
+}
+
+func TestDefined(t *testing.T) {
+	for _, pair := range testDefinedCases {
+		v := Defined(pair.data, pair.field)
+		if v != pair.result {
+			t.Error(
+				"For", pair.data,
+				"expected", pair.result,
+				"got", v,
+			)
+		}
+	}
+}
+
+type testSplitCapitalize struct {
+	name   string
+	result string
+}
+
+var testSplitCapitalizeCases = []testSplitCapitalize{
+	{"loremIpsum", "Lorem ipsum"},
+	{"LoremIpsum", "Lorem ipsum"},
+	{"loremipsum", "Loremipsum"},
+}
+
+func TestSplitCapitalize(t *testing.T) {
+	for _, pair := range testSplitCapitalizeCases {
+		v := SplitCapitalize(pair.name)
+		if v != pair.result {
+			t.Error(
+				"For", pair.name,
+				"expected", pair.result,
+				"got", v,
+			)
+		}
+	}
+}
