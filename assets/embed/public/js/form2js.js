e6e1a6c7d9da2a378e6fc817f9767a6869c8e7f5
Henrique Dias <hacdias@gmail.com>
2016-07-18 20:49:56 +0100

    fix form2js

--- assets/embed/public/js/form2js.js
+++ assets/embed/public/js/form2js.js
@@ -231,8 +231,8 @@
 	}
 
     function convertValue(value) {
-        if value == "true" return true;
-        if value == "false" return false;
+        if (value == "true") return true;
+        if (value == "false") return false;
         if (!isNaN(value)) return parseInt(value);
         return value;
     }
