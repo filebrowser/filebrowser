48ae058aab50bdefaab386776f78ad6023464475
Henrique Dias <hacdias@gmail.com>
2017-03-25 19:51:26 +0000

    update binary.sh
    Former-commit-id: e14e708d0a10e805085585c353ebdb7f93f9bf9d [formerly 04f2a120a4c232e77e6f8de0bf44897acb97065c] [formerly d3d31166f146ab3d0b6a765c2cb71948688da11e [formerly 3e9ee34e0bbe1ce2e17e9819a9298110058b9807]]
    Former-commit-id: ae2654bc6ba24bbe83a5ee3feec145bd382f2117 [formerly bfb6188b777a9a5242a8a26ca34a2aa1b5d4cfa8]
    Former-commit-id: 919f09697a3e99d215d07507208a3c1a42defc2c

--- binary.sh
+++ binary.sh
@@ -5,4 +5,6 @@ go get github.com/bountylabs/gitversion
 go-bindata -pkg assets -prefix "_embed" -o assets/binary.go _embed/templates/... _embed/public/js/... _embed/public/css/... _embed/public/ace/src-min/...
 gitversion -s -o page/version.go -p page
 
+gofmt -w page/version.go
+gofmt -w assets/binary.go
 git add -A
