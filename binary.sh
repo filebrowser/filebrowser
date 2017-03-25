f7598254c859e02bc884bd3ec173a165243d4171
Henrique Dias <hacdias@gmail.com>
2017-03-25 19:41:51 +0000

    subtree

--- binary.sh
+++ binary.sh
@@ -2,5 +2,5 @@
 
 go get github.com/jteeuwen/go-bindata/go-bindata
 go get github.com/bountylabs/gitversion
-go-bindata -pkg assets -prefix "_embed" -o assets/binary.go -ignore=.*ace.* _embed/... _embed/public/js/vendor/ace/src-min/...
+go-bindata -pkg assets -prefix "_embed" -o assets/binary.go _embed/templates/... _embed/public/js/... _embed/public/css/... _embed/public/ace/src-min/...
 gitversion -s -o page/version.go -p page
