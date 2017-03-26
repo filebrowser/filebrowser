191a3791e0edc4f474e9d86f85002d404a306710
Henrique Dias <hacdias@gmail.com>
2017-03-26 11:24:21 +0100

    pre-build.sh

--- filemanager.go
+++ filemanager.go
@@ -1,8 +1,3 @@
-//go:generate go get github.com/jteeuwen/go-bindata/go-bindata
-//go:generate go get github.com/bountylabs/gitversion
-//go:generate go-bindata -pkg assets -prefix "_embed" -o assets/binary.go _embed/templates/... _embed/public/js/... _embed/public/css/... _embed/public/ace/src-min/...
-//go:generate gitversion -s -o page/version.go -p page
-
 // Package filemanager provides middleware for managing files in a directory
 // when directory path is requested instead of a specific file. Based on browse
 // middleware.
