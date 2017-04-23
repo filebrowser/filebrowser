3877b7cf9047be5156d7f87f4f2a282968aef3d0
Henrique Dias <hacdias@gmail.com>
2017-04-23 15:41:26 +0100

    fix file

--- before_script.sh
+++ before_script.sh
@@ -3,8 +3,8 @@ set -e
 mkdir $GOPATH/src/caddy
 touch $GOPATH/src/caddy/main.go
 
-cat << 'EOF' >> $GOPATH/src/caddy/main.go
-package main\n
+cat << 'EOF' >> nada.tx
+package main
 import "github.com/mholt/caddy/caddy/caddymain"
 import "github.com/hacdias/caddy-hugo"
 func main() {
