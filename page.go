c67cb0cbfa27dc05afc03a6c75a507db2e3201c1
Henrique Dias <hacdias@gmail.com>
2017-06-26 20:38:40 +0100

    Sidebar and such
    Former-commit-id: ebced026b182699c6917c7a45cf4c9820ab5c71d [formerly 52e269ffe5f2ea87b2cd066c26572318d0686143] [formerly ae3fabb0a62e226d36bceee980a937f0445e6d0e [formerly ad0368b745298b32f76cb3c5db6ebc636349c993]]
    Former-commit-id: b975208dd1a279fc4a4c2b988523385fccb9d517 [formerly 28205475b615611c29f3ba2fc447ba4c1290c25b]
    Former-commit-id: 8fea88e559f2712d37a7c58261f50c376e1f0a56

--- page.go
+++ page.go
@@ -103,6 +103,7 @@ func (p page) PreviousLink() string {
 
 // PrintAsHTML formats the page in HTML and executes the template
 func (p page) PrintAsHTML(w http.ResponseWriter, m *FileManager, templates ...string) (int, error) {
+	templates = append(templates, "actions")
 	templates = append(templates, "templates")
 
 	if p.minimal {
