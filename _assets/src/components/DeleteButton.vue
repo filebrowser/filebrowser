3eb9505ec490d1157ea614422e69499ed7c729fc
Henrique Dias <hacdias@gmail.com>
2017-06-28 22:24:00 +0100

    add delete button
    Former-commit-id: 2f4c852246863df64da176c58ada58ce3f445f18 [formerly 261760e95a81f32882e7bf3b57d6012c63911230] [formerly d2a6c7600bbdd51f717ab10c0310beb0ae4c45a6 [formerly cc917a621ee4f0d67f6201ba4007f7e69ec1003c]]
    Former-commit-id: 6fe869e23eb2393ab2527d5585faff5755d10741 [formerly c67901bc11bacd4101a89cb1e7c3936fe4b88bb7]
    Former-commit-id: 7e39134924a109b64b089ff76ac0777f9ed9e2cb

---
+++ _assets/src/components/DeleteButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button @click="show" aria-label="Delete"  title="Delete" class="action" id="delete">
+        <i class="material-icons">delete</i>
+        <span>Delete</span>
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'delete-button',
+  methods: {
+    show: function (event) {
+      window.info.showDelete = true
+    }
+  }
+}
+</script>
