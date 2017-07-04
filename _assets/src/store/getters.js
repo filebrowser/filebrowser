f0e9b2a6339a56196cf0a9b4abd2e7f20def2ae0
Henrique Dias <hacdias@gmail.com>
2017-07-04 09:01:27 +0100

    Put some getters function in their scope

--- _assets/src/store/getters.js
+++ _assets/src/store/getters.js
@@ -1,13 +1,4 @@
 const getters = {
-  showInfo: state => (state.prompt === 'info'),
-  showHelp: state => (state.prompt === 'help'),
-  showDelete: state => (state.prompt === 'delete'),
-  showRename: state => (state.prompt === 'rename'),
-  showMove: state => (state.prompt === 'move'),
-  showNewFile: state => (state.prompt === 'newFile'),
-  showNewDir: state => (state.prompt === 'newDir'),
-  showDownload: state => (state.prompt === 'download'),
-  showOverlay: state => (state.prompt !== null),
   selectedCount: state => state.selected.length
 }
 
