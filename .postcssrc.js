eaacf59c3179ec481b07eebfa1b9311a8a64bb28
Henrique Dias <hacdias@gmail.com>
2017-07-06 08:30:04 +0100

    Put POSTCSS config in package.json
    Former-commit-id: 73733038fa12ac37c64e5767a0998cd3707935de [formerly c9fc364c4b74a6bc946b33609628c1359a85b7a4] [formerly 628dc43cd8d7c8eb6efb00e59091637f5b4b8f09 [formerly 1b6b7d18818e57c2594266079b16aec5e6f4bd2c]]
    Former-commit-id: aa27dc11ffa723de8c20cd9f371e347f89ff307b [formerly f4597ce0a4e382d578468097ae0e239c3cca584f]
    Former-commit-id: 84248423c02764d6c8f9254eaaa6aa439b9c3e22

--- .postcssrc.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-
-module.exports = {
-  "plugins": {
-    // to edit target browsers: use "browserlist" field in package.json
-    "autoprefixer": {}
-  }
-}
