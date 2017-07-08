c499c502d1454ef7536de2410575ffe7565775d1
Henrique Dias <hacdias@gmail.com>
2017-07-08 18:13:19 +0100

    Globsl Settings
    Former-commit-id: 855a63173ad55dc5e57b5670bd431a58aefbae63 [formerly cdb2474aadc0db47c82631974681ace970034ebd] [formerly f92f0debbc0c31e716b666481153b83e8ec87067 [formerly 8c019921ba10b17c579b16a292cca9d342e4f476]]
    Former-commit-id: c0bce62c5c628aadbc4388bfa4e15adf39f7290b [formerly b1ed038ac0824615f78a2fa3893e2c7c9f3756ab]
    Former-commit-id: bcdb6e54e5bf65b642afd25925b73e00e6daec18

--- assets/src/components/GlobalSettings.vue
+++ assets/src/components/GlobalSettings.vue
@@ -6,9 +6,19 @@
       <li><router-link v-if="user.admin" to="/users">Go to User Management</router-link></li>
     </ul>
 
-    <form>
-      <h2>Before Save</h2>
-      <textarea v-model="css" name="css"></textarea>
+    <form @submit="saveHooks">
+      <h2>Commands</h2>
+
+      <p class="small">Here you can set commands that are executed in the named events. You write one command
+        per line. If the event is related to files, such as before and after saving, the environment variable
+        <code>file</code> will be available with the path of the file.</p>
+
+      <h3>Before Save</h3>
+      <textarea v-model="beforeSave"></textarea>
+
+      <h3>After Save</h3>
+      <textarea v-model="afterSave"></textarea>
+
       <p><input type="submit" value="Save"></p>
     </form>
 
@@ -22,13 +32,21 @@ export default {
   name: 'settings',
   data: function () {
     return {
+      beforeSave: '',
+      afterSave: ''
     }
   },
   computed: {
     ...mapState([ 'user' ])
   },
+  created () {
+    // TODO: fetch current settings here
+  },
   methods: {
-    ...mapMutations([ 'showSuccess' ])
+    ...mapMutations([ 'showSuccess' ]),
+    saveHooks (event) {
+      event.preventDefault()
+    }
   }
 }
 </script>
