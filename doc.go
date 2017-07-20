2cdd901f23c2981c3a991233cc2538fac6917d7f
Henrique Dias <hacdias@gmail.com>
2017-07-20 09:32:42 +0100

    docs [ci skip]

---
+++ doc.go
@@ -0,0 +1,10 @@
+/*
+Package filemanager provides a web interface to access
+your files wherever you are.
+
+To use this package, you'll need to start by creating a
+filemanager instance:
+
+	fm, err := New(database, user)
+*/
+package filemanager
