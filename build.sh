81b1e8d4305e7dd019cd8a6bb70b3a3c76a62283
Henrique Dias <hacdias@gmail.com>
2017-07-28 11:21:55 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+# Install rice tool if not present
+if ! [ -x "$(command -v rice)" ]; then
+  go get github.com/GeertJohan/go.rice/rice
+fi
+
+# Clean the dist folder and build the assets
 rm -rf assets/dist
 npm run build
+
+# Embed the assets using rice
 rice embed-go
 cd ./caddy/hugo
 rice embed-go
