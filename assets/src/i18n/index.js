d219a3bc43c2e37f490ff4bed3a968e5af22f746
Henrique Dias <hacdias@gmail.com>
2017-07-31 19:45:33 +0100

    Add Simplified Chinese and sort by alphabet

--- assets/src/i18n/index.js
+++ assets/src/i18n/index.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import en from './en.yaml'
 import pt from './pt.yaml'
+import zhCN from './zh_cn.yaml'
 
 Vue.use(VueI18n)
 
 export default new VueI18n({
-  locale: 'en',
+  locale: 'zh_cn',
   fallbackLocale: 'en',
   messages: {
     'en': en,
-    'pt': pt
+    'pt': pt,
+    'zh_cn': zhCN
   }
 })
