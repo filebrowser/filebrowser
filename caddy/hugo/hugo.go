4071a58107b34e7097deac3d452f84fe88404a0f
Henrique Dias <hacdias@gmail.com>
2017-08-14 18:35:25 +0100

    close #203
    Former-commit-id: e2d0b723963d3a82ac0f9042280885e800b1132a [formerly e1ae3f4da43f0481a57c39f11735c36b33fda857] [formerly 02ab5f5fb5f4b812cf413ebc923b853d4f0b4afb [formerly 83bc5550942b7f11e65bc2283b3a54e201da60d7]]
    Former-commit-id: bee2ec30c9aa9619a69eaa6320822f8525a41535 [formerly 1af59077494b5e8674af547d0361f50a2ecf8f26]
    Former-commit-id: 573870f4a6bffcee3e48fbc0b8349402eaeba407

--- caddy/hugo/hugo.go
+++ caddy/hugo/hugo.go
@@ -1,168 +1,13 @@
 package hugo
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"fmt"
 	"net/http"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
 
 	"github.com/hacdias/filemanager"
-	"github.com/hacdias/fileutils"
+	"github.com/hacdias/filemanager/caddy/parser"
 	"github.com/mholt/caddy"
 	"github.com/mholt/caddy/caddyhttp/httpserver"
 )
-
-// setup configures a new FileManager middleware instance.
-func setup(c *caddy.Controller) error {
-	configs, err := parse(c)
-	if err != nil {
-		return err
-	}
-
-	httpserver.GetConfig(c).AddMiddleware(func(next httpserver.Handler) httpserver.Handler {
-		return plugin{Configs: configs, Next: next}
-	})
-
-	return nil
-}
-
-func parse(c *caddy.Controller) ([]*filemanager.FileManager, error) {
-	var (
-		configs []*filemanager.FileManager
-	)
-
-	for c.Next() {
-		// hugo [directory] [admin] {
-		// 		database path
-		// }
-		directory := "."
-		admin := "/admin"
-		database := ""
-		noAuth := false
-
-		// Get the baseURL and baseScope
-		args := c.RemainingArgs()
-
-		if len(args) >= 1 {
-			directory = args[0]
-		}
-
-		if len(args) > 1 {
-			admin = args[1]
-		}
-
-		for c.NextBlock() {
-			switch c.Val() {
-			case "database":
-				if !c.NextArg() {
-					return nil, c.ArgErr()
-				}
-
-				database = c.Val()
-			case "no_auth":
-				if !c.NextArg() {
-					noAuth = true
-					continue
-				}
-
-				var err error
-				noAuth, err = strconv.ParseBool(c.Val())
-				if err != nil {
-					return nil, err
-				}
-			}
-		}
-
-		caddyConf := httpserver.GetConfig(c)
-
-		path := filepath.Join(caddy.AssetsPath(), "hugo")
-		err := os.MkdirAll(path, 0700)
-		if err != nil {
-			return nil, err
-		}
-
-		// if there is a database path and it is not absolute,
-		// it will be relative to ".caddy" folder.
-		if !filepath.IsAbs(database) && database != "" {
-			database = filepath.Join(path, database)
-		}
-
-		// If there is no database path on the settings,
-		// store one in .caddy/hugo/{name}.db.
-		if database == "" {
-			// The name of the database is the hashed value of a string composed
-			// by the host, address path and the baseurl of this File Manager
-			// instance.
-			hasher := md5.New()
-			hasher.Write([]byte(caddyConf.Addr.Host + caddyConf.Addr.Path + admin))
-			sha := hex.EncodeToString(hasher.Sum(nil))
-			database = filepath.Join(path, sha+".db")
-
-			fmt.Println("[WARNING] A database is going to be created for your Hugo instace at " + database +
-				". It is highly recommended that you set the 'database' option to '" + sha + ".db'\n")
-		}
-
-		m, err := filemanager.New(database, filemanager.User{
-			Locale:        "en",
-			AllowCommands: true,
-			AllowEdit:     true,
-			AllowNew:      true,
-			AllowPublish:  true,
-			Commands:      []string{"git", "svn", "hg"},
-			Rules: []*filemanager.Rule{{
-				Regex:  true,
-				Allow:  false,
-				Regexp: &filemanager.Regexp{Raw: "\\/\\..+"},
-			}},
-			CSS:        "",
-			FileSystem: fileutils.Dir(directory),
-		})
-
-		if err != nil {
-			return nil, err
-		}
-
-		// Initialize the default settings for Hugo.
-		hugo := &filemanager.Hugo{
-			Root:        directory,
-			Public:      filepath.Join(directory, "public"),
-			Args:        []string{},
-			CleanPublic: true,
-		}
-
-		// Attaches Hugo plugin to this file manager instance.
-		err = m.EnableStaticGen(hugo)
-		if err != nil {
-			return nil, err
-		}
-
-		m.NoAuth = noAuth
-		m.SetBaseURL(admin)
-		m.SetPrefixURL(strings.TrimSuffix(caddyConf.Addr.Path, "/"))
-		configs = append(configs, m)
-	}
-
-	return configs, nil
-}
-
-// ServeHTTP determines if the request is for this plugin, and if all prerequisites are met.
-func (p plugin) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
-	for i := range p.Configs {
-		// Checks if this Path should be handled by File Manager.
-		if !httpserver.Path(r.URL.Path).Matches(p.Configs[i].BaseURL) {
-			continue
-		}
-
-		p.Configs[i].ServeHTTP(w, r)
-		return 0, nil
-	}
-
-	return p.Next.ServeHTTP(w, r)
-}
 
 func init() {
 	caddy.RegisterPlugin("hugo", caddy.Plugin{
@@ -174,4 +19,33 @@ func init() {
 type plugin struct {
 	Next    httpserver.Handler
 	Configs []*filemanager.FileManager
+}
+
+// ServeHTTP determines if the request is for this plugin, and if all prerequisites are met.
+func (f plugin) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
+	for i := range f.Configs {
+		// Checks if this Path should be handled by File Manager.
+		if !httpserver.Path(r.URL.Path).Matches(f.Configs[i].BaseURL) {
+			continue
+		}
+
+		f.Configs[i].ServeHTTP(w, r)
+		return 0, nil
+	}
+
+	return f.Next.ServeHTTP(w, r)
+}
+
+// setup configures a new FileManager middleware instance.
+func setup(c *caddy.Controller) error {
+	configs, err := parser.Parse(c, "hugo")
+	if err != nil {
+		return err
+	}
+
+	httpserver.GetConfig(c).AddMiddleware(func(next httpserver.Handler) httpserver.Handler {
+		return plugin{Configs: configs, Next: next}
+	})
+
+	return nil
 }
