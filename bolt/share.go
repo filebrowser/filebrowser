c6e6b0830505724aab779859c56a9cdb55fa9016
Henrique Dias <hacdias@gmail.com>
2017-08-20 09:27:03 +0100

    Add Comments

--- bolt/share.go
+++ bolt/share.go
@@ -6,10 +6,12 @@ import (
 	fm "github.com/hacdias/filemanager"
 )
 
+// ShareStore is a shareable links store.
 type ShareStore struct {
 	DB *storm.DB
 }
 
+// Get gets a Share Link from an hash.
 func (s ShareStore) Get(hash string) (*fm.ShareLink, error) {
 	var v *fm.ShareLink
 	err := s.DB.One("Hash", hash, &v)
@@ -20,6 +22,7 @@ func (s ShareStore) Get(hash string) (*fm.ShareLink, error) {
 	return v, err
 }
 
+// GetPermanent gets the permanent link from a path.
 func (s ShareStore) GetPermanent(path string) (*fm.ShareLink, error) {
 	var v *fm.ShareLink
 	err := s.DB.Select(q.Eq("Path", path), q.Eq("Expires", false)).First(&v)
@@ -30,6 +33,7 @@ func (s ShareStore) GetPermanent(path string) (*fm.ShareLink, error) {
 	return v, err
 }
 
+// GetByPath gets all the links for a specific path.
 func (s ShareStore) GetByPath(hash string) ([]*fm.ShareLink, error) {
 	var v []*fm.ShareLink
 	err := s.DB.Find("Path", hash, &v)
@@ -40,16 +44,19 @@ func (s ShareStore) GetByPath(hash string) ([]*fm.ShareLink, error) {
 	return v, err
 }
 
+// Gets retrieves all the shareable links.
 func (s ShareStore) Gets() ([]*fm.ShareLink, error) {
 	var v []*fm.ShareLink
 	err := s.DB.All(&v)
 	return v, err
 }
 
+// Save stores a Share Link on the database.
 func (s ShareStore) Save(l *fm.ShareLink) error {
 	return s.DB.Save(l)
 }
 
+// Delete deletes a Share Link from the database.
 func (s ShareStore) Delete(hash string) error {
 	return s.DB.DeleteStruct(&fm.ShareLink{Hash: hash})
 }
