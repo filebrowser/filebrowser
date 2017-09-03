1cfd31756da7d70c7d7552a49ea15561a08afd6d
Henrique Dias <hacdias@gmail.com>
2017-09-03 10:51:45 +0100

    untracked version Sun, Sep  3, 2017 10:51:45 AM
    Former-commit-id: 3f9a066d3456fd0435b5df19a6071f403761c363 [formerly 7ed9f0a686c30d97af32dfb7fa35b1f4dbbcc9ca] [formerly 8f473d98da1799211e7e7fd4e26dc9481297ea7d [formerly c60e7b9c0a477f2f3d730589f8fd4644f54b2296]]
    Former-commit-id: 732ae3300efa4444b06406743f9b8cce813e6b99 [formerly 465966ff48d99ca33496ff9dcf2d51e375985d0f]
    Former-commit-id: 8c61fc2054cbecd2f0b7354884634f4b73f6e77f

--- filemanager.go
+++ filemanager.go
@@ -21,7 +21,7 @@ import (
 )
 
 // Version is the current File Manager version.
-const Version = "1.3.1"
+const Version = "(untracked)"
 
 var (
 	ErrExist              = errors.New("the resource already exists")
