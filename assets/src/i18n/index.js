54c714ffd214fac0a6bd13aeb2d6fd85b78ce33e
Iñigo <igargi21@hotmail.com>
2017-12-04 16:28:57 +0100

    Add Spanish

--- assets/src/i18n/index.js
+++ assets/src/i18n/index.js
@@ -6,6 +6,7 @@ import pt from './pt.yaml'
 import ja from './ja.yaml'
 import zhCN from './zh-cn.yaml'
 import zhTW from './zh-tw.yaml'
+import es from './es.yaml'
 
 Vue.use(VueI18n)
 
@@ -33,6 +34,9 @@ export function detectLocale () {
     case /^zh.*/i.test(locale):
       locale = 'zh-cn'
       break
+    case /^es.*/i.test(locale):
+      locale = 'es'
+      break
     default:
       locale = 'en'
   }
@@ -49,7 +53,8 @@ const i18n = new VueI18n({
     'pt': pt,
     'ja': ja,
     'zh-cn': zhCN,
-    'zh-tw': zhTW
+    'zh-tw': zhTW,
+    'es': es
   }
 })
 
