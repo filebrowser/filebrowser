f91ee24858116d7a313bdf8d57a994abbbc3f2a8
1138-4EB <1138-4EB@users.noreply.github.com>
2018-04-18 17:27:28 +0200

    drop package.json, use yarn instad of npm, add build_in_docker.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 set -e
+
+cd $(dirname $0)
+
+# Clean the dist folder and build the assets
+if [ ! -d "frontend" ]; then
+  git clone -b master https://github.com/filebrowser/frontend
+fi;
+cd frontend
+if [ -d "dist" ]; then
+  rm -rf dist/*
+fi;
+yarn install
+yarn build
+
+cd ..
 
 # Install rice tool if not present
 if ! [ -x "$(command -v rice)" ]; then
   go get github.com/GeertJohan/go.rice/rice
 fi
-
-# Clean the dist folder and build the assets
-rm -rf node_modules
-npm install
 
 # Embed the assets using rice
 rice embed-go
