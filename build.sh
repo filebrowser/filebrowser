f9e3923ae799dcc011bbda0aa31a977094fcf064
1138-4EB <1138-4EB@users.noreply.github.com>
2018-06-06 20:55:18 +0100

    add filebrowser/frontend as a submodule
    Former-commit-id: 689395356dc1860d0f0f5786986cbbc4238a1fe0 [formerly 0c528dae26624fad221c4fa6b273a02dc030e81c] [formerly f353338b249c2ed818611ac6a91939bc87356de9 [formerly a2e293292ec9f946d14a47d16f6741f2b28d0c8c]]
    Former-commit-id: 647dac9f8eed2df57191a75e4b199aebb18375fc [formerly 000079405f67a4bff2098c2cddc082a4374e0bd5]
    Former-commit-id: 89c911c807783e2176994fc6949bf8221ca5e5a3

--- build.sh
+++ build.sh
@@ -5,16 +5,12 @@ set -e
 cd $(dirname $0)
 
 # Clean the dist folder and build the assets
-if [ ! -d "frontend" ]; then
-  git clone -b master https://github.com/filebrowser/frontend
-fi;
 cd frontend
 if [ -d "dist" ]; then
   rm -rf dist/*
 fi;
 yarn install
 yarn build
-
 cd ..
 
 dep ensure -vendor-only
