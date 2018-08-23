3ae9e518a30ec59e307f60f0225b8eb04433be08
1138-4EB <1138-4EB@users.noreply.github.com>
2018-08-24 00:20:20 +0100

    fix: lowercase BaseURL (filebrowser/filebrowser#522)

--- build/config.js
+++ build/config.js
@@ -5,7 +5,7 @@ module.exports = {
   index: path.resolve(__dirname, '../dist/index.html'),
   assetsRoot: path.resolve(__dirname, '../dist'),
   assetsSubDirectory: 'static',
-  assetsPublicPath: '{{ .BaseURL }}/',
+  assetsPublicPath: '{{ .baseurl }}/',
   build: {
     env: {
       NODE_ENV: '"production"'
