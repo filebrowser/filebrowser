b344efef2dd9171ecbf10f0f19229a408dda0bae
Henrique Dias <hacdias@gmail.com>
2018-12-28 19:19:54 +0000

    fix: move everything to LF (#588)
    * fix: move everything to LF
    
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>
    
    * feat: metalinter
    
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- build/build_all.sh
+++ build/build_all.sh
@@ -25,8 +25,6 @@ if [ "$USE_DOCKER" != "" ]; then
     filebrowser/dev \
     sh -c "\
       cd build && \
-      dos2unix build_assets.sh && \
-      dos2unix build.sh && \
       ./build_assets.sh && \
       ./build.sh \
     "
