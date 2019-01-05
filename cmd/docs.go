2a434727e0c7db0e0c67e06717df48f08fff1ef2
Henrique Dias <hacdias@gmail.com>
2019-01-05 14:54:06 +0000

    feat: do not depend on cobra's doc gen
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- cmd/docs.go
+++ cmd/docs.go
@@ -1,16 +1,15 @@
 package cmd
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 	"path/filepath"
 	"sort"
 	"strings"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/doc"
 )
 
 func init() {
@@ -18,17 +17,7 @@ func init() {
 	docsCmd.Flags().StringP("path", "p", "./docs", "path to save the docs")
 }
 
-func removeAll (dir string) error {
-	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if strings.HasPrefix(info.Name(), "filebrowser") {
-			return os.Remove(path)
-		}
-
-		return nil
-	})
-}
-
-func printToc (names []string) {
+func printToc(names []string) {
 	for i, name := range names {
 		name = strings.TrimSuffix(name, filepath.Ext(name))
 		name = strings.Replace(name, "-", " ", -1)
@@ -42,7 +31,7 @@ func printToc (names []string) {
 		toc += "* [" + name + "](cli/" + strings.Replace(name, " ", "-", -1) + ".md)\n"
 	}
 
-	fmt.Println(toc) 
+	fmt.Println(toc)
 }
 
 var docsCmd = &cobra.Command{
@@ -51,15 +40,10 @@ var docsCmd = &cobra.Command{
 	Args:   cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		dir := mustGetString(cmd, "path")
-
-		err := removeAll(dir)
-		checkErr(err)
-		err = doc.GenMarkdownTree(rootCmd, dir)
-		checkErr(err)
-		
+		generateDocs(rootCmd, dir)
 		names := []string{}
 
-		err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 			if err != nil || info.IsDir() {
 				return err
 			}
@@ -68,39 +52,78 @@ var docsCmd = &cobra.Command{
 				return nil
 			}
 
-			name := strings.Replace(info.Name(), "_", "-", -1)
-			names = append(names, name)
-			newPath := filepath.Join(dir, name)
-			err = os.Rename(path, newPath)
-			if err != nil {
-				return err
-			}
-			path = newPath
-
-			fd, err := os.OpenFile(path, os.O_RDONLY, os.ModePerm)
-			if err != nil {
-				return err
-			}
-			defer fd.Close()
-
-			content := ""
-			sc := bufio.NewScanner(fd)
-			for sc.Scan() {
-				txt := sc.Text()
-				if txt == "### SEE ALSO" {
-					break
-				}
-				content += txt + "\n"
-			}
-			if err := sc.Err(); err != nil {
-				return err
-			}
-
-			content = strings.TrimSpace(content) + "\n"
-			return ioutil.WriteFile(path, []byte(content), 077)
+			names = append(names, info.Name())
+			return nil
 		})
 
 		checkErr(err)
 		printToc(names)
 	},
+}
+
+func generateDocs(cmd *cobra.Command, dir string) {
+	for _, c := range cmd.Commands() {
+		if !c.IsAvailableCommand() || c.IsAdditionalHelpTopicCommand() {
+			continue
+		}
+
+		generateDocs(c, dir)
+	}
+
+	basename := strings.Replace(cmd.CommandPath(), " ", "-", -1) + ".md"
+	filename := filepath.Join(dir, basename)
+	f, err := os.Create(filename)
+	checkErr(err)
+	defer f.Close()
+	generateMarkdown(cmd, f)
+}
+
+func generateMarkdown(cmd *cobra.Command, w io.Writer) {
+	cmd.InitDefaultHelpCmd()
+	cmd.InitDefaultHelpFlag()
+
+	buf := new(bytes.Buffer)
+	name := cmd.CommandPath()
+
+	short := cmd.Short
+	long := cmd.Long
+	if len(long) == 0 {
+		long = short
+	}
+
+	buf.WriteString("# " + name + "\n\n")
+	buf.WriteString(short + "\n\n")
+	buf.WriteString("## Synopsis\n\n")
+	buf.WriteString(long + "\n\n")
+
+	if cmd.Runnable() {
+		buf.WriteString(fmt.Sprintf("```\n%s\n```\n\n", cmd.UseLine()))
+	}
+
+	if len(cmd.Example) > 0 {
+		buf.WriteString("## Examples\n\n")
+		buf.WriteString(fmt.Sprintf("```\n%s\n```\n\n", cmd.Example))
+	}
+
+	printOptions(buf, cmd, name)
+	_, err := buf.WriteTo(w)
+	checkErr(err)
+}
+
+func printOptions(buf *bytes.Buffer, cmd *cobra.Command, name string) {
+	flags := cmd.NonInheritedFlags()
+	flags.SetOutput(buf)
+	if flags.HasAvailableFlags() {
+		buf.WriteString("## Options\n\n```\n")
+		flags.PrintDefaults()
+		buf.WriteString("```\n\n")
+	}
+
+	parentFlags := cmd.InheritedFlags()
+	parentFlags.SetOutput(buf)
+	if parentFlags.HasAvailableFlags() {
+		buf.WriteString("## Options inherited from parent commands\n\n```\n")
+		parentFlags.PrintDefaults()
+		buf.WriteString("```\n")
+	}
 }
