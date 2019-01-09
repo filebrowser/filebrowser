8df0715064e1fb5efd047f29aabee3cd03082fa2
Henrique Dias <hacdias@gmail.com>
2019-01-09 07:50:59 +0000

    fix: optimization already done by the underlying code
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- http/http.go
+++ http/http.go
@@ -59,9 +59,5 @@ func NewHandler(storage *storage.Storage, server *settings.Server) (http.Handler
 	public.PathPrefix("/dl").Handler(monkey(publicDlHandler, "/api/public/dl/")).Methods("GET")
 	public.PathPrefix("/share").Handler(monkey(publicShareHandler, "/api/public/share/")).Methods("GET")
 
-	if server.BaseURL == "" {
-		return r, nil
-	}
-
 	return http.StripPrefix(server.BaseURL, r), nil
 }
