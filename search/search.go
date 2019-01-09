b578e2196aff1f88df54048f48ffef346cac92df
Henrique Dias <hacdias@gmail.com>
2019-01-09 14:18:03 +0000

    fix: search (#625)
    Former-commit-id: d9c9234e87c190d847572c1fe715cf745ba44b53 [formerly f8b5f600fe39be8b47aece870ff24756c5dcfc6d] [formerly ad1cbfd739888d4cc3a1db882a98251cac3b89cc [formerly bafe9f0ad7b400f55590b6d448f87cd9d16c6ece]]
    Former-commit-id: 493e4f3e4b42092201cfd1f99d8ee6cd3eb25dfc [formerly 520a9df6582492f24906f8f3f5c5e90cf5a02acd]
    Former-commit-id: 17484d0fe391a82d5ee0dc990f4b1f039d305f4c

--- search/search.go
+++ search/search.go
@@ -18,16 +18,23 @@ type searchOptions struct {
 func Search(fs afero.Fs, scope, query string, checker rules.Checker, found func(path string, f os.FileInfo) error) error {
 	search := parseSearch(query)
 
-	return afero.Walk(fs, scope, func(path string, f os.FileInfo, err error) error {
-		path = strings.TrimPrefix(path, "/")
-		path = strings.Replace(path, "\\", "/", -1)
+	scope = strings.Replace(scope, "\\", "/", -1)
+	scope = strings.TrimPrefix(scope, "/")
+	scope = strings.TrimSuffix(scope, "/")
+	scope = "/" + scope + "/"
 
-		if !checker.Check(path) {
+	return afero.Walk(fs, scope, func(originalPath string, f os.FileInfo, err error) error {
+		originalPath = strings.Replace(originalPath, "\\", "/", -1)
+		originalPath = strings.TrimPrefix(originalPath, "/")
+		originalPath = "/" + originalPath
+		path := originalPath
+
+		if path == scope {
 			return nil
 		}
 
-		if !search.CaseSensitive {
-			path = strings.ToLower(path)
+		if !checker.Check(path) {
+			return nil
 		}
 
 		if !search.CaseSensitive {
@@ -52,7 +59,7 @@ func Search(fs afero.Fs, scope, query string, checker rules.Checker, found func(
 		if len(search.Terms) > 0 {
 			for _, term := range search.Terms {
 				if strings.Contains(path, term) {
-					return found(strings.TrimPrefix(path, scope), f)
+					return found(strings.TrimPrefix(originalPath, scope), f)
 				}
 			}
 		}
