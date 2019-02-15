1259fc1bbceaffd094f29fc13ee1e21d0e668750
Henrique Dias <hacdias@gmail.com>
2019-02-15 12:54:44 +0100

    feat: remove version cmd (#675)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>
    Former-commit-id: 04a60c58f20d63ca7b25731c41e144bcf0f538cc [formerly e6e179799b64779515051df53352df5e63edc259] [formerly 0689eba81ff5f7ee3ea75db37b91cef4d6d8f35c [formerly 85899acae6edc445506384a5fa2c972131cb06e6]]
    Former-commit-id: 96ca0cadb94131ddd3b57f0f11ad629edf687e40 [formerly 50130c75d39e67b15a645e7f4879acf34a5d6620]
    Former-commit-id: 53b8120673a82217c3625de161d4ec57a96e1470

--- cmd/hash.go
+++ cmd/hash.go
@@ -12,11 +12,10 @@ func init() {
 }
 
 var hashCmd = &cobra.Command{
-	Use:     "hash <password>",
-	Version: rootCmd.Version,
-	Short:   "Hashes a password",
-	Long:    `Hashes a password using bcrypt algorithm.`,
-	Args:    cobra.ExactArgs(1),
+	Use:   "hash <password>",
+	Short: "Hashes a password",
+	Long:  `Hashes a password using bcrypt algorithm.`,
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		pwd, err := users.HashPwd(args[0])
 		checkErr(err)
