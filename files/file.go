586bb63ee708416c8aa343bfaa2d69eeb1e2bc85
荒野無燈 <i@80x86.io>
2019-05-08 02:02:12 +0800

    fixup func isBinary to handle CJK runes correctly
    Former-commit-id: 880817e9e688f7126eb5e3010f5fc37110b28448 [formerly d44d541d75c625a474ca7c8f9adcc52e20ee69e0] [formerly fbbaf7b6a31ed09944700f1ffa98c0baca9ae0f3 [formerly 1c4253952259cd1efe36064fb1d96e782b0d7501]]
    Former-commit-id: 2a647332f2d80741f7ac9cd4eccffbf8a0dd5348 [formerly ef7355350d4d1276911bcdd8b4aaaf1b82efe50c]
    Former-commit-id: 7c6d116b6bda492cc9544dc4a46d95cd225c1b39

--- files/file.go
+++ files/file.go
@@ -164,7 +164,7 @@ func (i *FileInfo) detectType(modify, saveContent bool) error {
 	case strings.HasPrefix(mimetype, "image"):
 		i.Type = "image"
 		return nil
-	case isBinary(string(buffer[:n])) || i.Size > 10*1024*1024: // 10 MB
+	case isBinary(buffer[:n], n) || i.Size > 10*1024*1024: // 10 MB
 		i.Type = "blob"
 		return nil
 	default:
