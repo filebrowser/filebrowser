3ab225a101fc86135de5b2ed5e7080b72572643b
Henrique Dias <hacdias@gmail.com>
2019-05-11 19:35:50 +0100

    chore: remove caddy (#724)
    feat: remove caddy bug repo
    
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>
    
    Former-commit-id: 4345a1d1b37df8b2d626c12e30415e13f70d5127 [formerly cbee013f5a8550dcd02efa3b3dc9a753126d5324] [formerly 6af0fc7ef5f751265163b91c5f7d004c822d72ae [formerly 0a37918677de5e539cad95af2323d34e6a6c2d2d]]
    Former-commit-id: f4493baf307b933be0fc9faec048ba0a707b0cdb [formerly 5dd46cd82f833b56fe5ef6017e7eaeaea6eb3ceb]
    Former-commit-id: efecca4f50951099c2dea55bd904ce9077c21a0c

--- wizard.sh
+++ wizard.sh
@@ -121,46 +121,6 @@ lint () {
   fi
 }
 
-pushRicebox () {
-  COMMIT_SHA="$(git rev-parse --verify HEAD | cut -c1-8)"
-
-  cd $REPO
-
-  eval `ssh-agent -s`
-  openssl aes-256-cbc -K $encrypted_9ca81b5594f5_key -iv $encrypted_9ca81b5594f5_iv -in ./.ci/deploy_key.enc -d | ssh-add -
-
-  git clone git@github.com:filebrowser/caddy caddy
-  cd caddy
-  cp ../http/rice-box.go ./
-  sed -i 's/package http/package caddy/g' ./rice-box.go
-
-  go get -u github.com/filebrowser/filebrowser/v2@latest
-
-  git checkout -b update-rice-box origin/master
-  git config --local user.name "Filebrowser Bot"
-  git config --local user.email "FilebrowserBot@users.noreply.github.com"
-  git commit -am "update rice-box $COMMIT_SHA"
-
-  if [ $(git tag | grep "$TRAVIS_TAG" | wc -l) -ne 0 ]; then
-    git tag -d "$TRAVIS_TAG"
-  fi
-
-  git tag "$TRAVIS_TAG"
-
-  if [ "$(git ls-remote --heads origin update-rice-box)" != "" ]; then
-    git push -u origin update-rice-box
-  else
-    git push origin +update-rice-box
-  fi
-
-  if [ "$(git ls-remote --heads origin update-rice-box)" != "" ]; then
-    git push origin "$TRAVIS_TAG"
-  else
-    git push origin :"$TRAVIS_TAG"
-    git push origin "$TRAVIS_TAG"
-  fi
-}
-
 ciRelease () {
   docker run --rm -t \
     -v $(pwd):/src \
@@ -172,7 +132,6 @@ ciRelease () {
       goreleaser \
     "
 
-  pushRicebox
   dockerPushTag
 }
 
