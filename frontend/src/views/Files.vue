b521dec8f9b14dd92248c429e902ebc639046389
Ramires Viana <59319979+ramiresviana@users.noreply.github.com>
2021-03-24 12:23:05 +0000

    fix: hidden editor header on Safari

--- frontend/src/views/Files.vue
+++ frontend/src/views/Files.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header-bar v-if="error || !req.type" showMenu showLogo />
+    <header-bar v-if="error || req.type == null" showMenu showLogo />
 
     <breadcrumbs base="/files" />
 
