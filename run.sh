6bd76f97d60b0069c1f0e7e8f0bd075460010e74
Osmany Montero <osmontero@icloud.com>
2021-06-03 21:09:56 +0300

    no auth

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 cd /
 chmod 777 filebrowser
 ./filebrowser config init
+./filebrowser config set --auth.method=noauth
 ./filebrowser users add admin ${PASSWORD}
 ./filebrowser -a 0.0.0.0 -p 80 -b /srv
