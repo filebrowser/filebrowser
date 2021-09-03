94d59c273778a7df44b708e100297cae10d36444
lihengxin <lihengxin@baidu.com>
2021-09-03 17:09:02 +0800

    Mod modified time format

--- frontend/src/components/files/ListingItem.vue
+++ frontend/src/components/files/ListingItem.vue
@@ -106,7 +106,7 @@ export default {
       return filesize(this.size);
     },
     humanTime: function () {
-      return moment(this.modified).fromNow();
+      return moment(this.modified).format("YYYY-MM-DD HH:mm");
     },
     dragStart: function () {
       if (this.selectedCount === 0) {
