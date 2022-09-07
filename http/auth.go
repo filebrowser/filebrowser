6d3916252bb346dfa268bc88993f74eab655157a
lihengxin <lihengxin.123@bytedance.com>
2022-09-07 14:31:47 +0800

    feat(editor): Add Editor Auto Save
    Change-Id: I9ce890d9f9a264582f26a1d4cb4fcc0a924ae949

--- http/auth.go
+++ http/auth.go
@@ -20,15 +20,16 @@ const (
 )
 
 type userInfo struct {
-	ID           uint              `json:"id"`
-	Locale       string            `json:"locale"`
-	ViewMode     users.ViewMode    `json:"viewMode"`
-	SingleClick  bool              `json:"singleClick"`
-	Perm         users.Permissions `json:"perm"`
-	Commands     []string          `json:"commands"`
-	LockPassword bool              `json:"lockPassword"`
-	HideDotfiles bool              `json:"hideDotfiles"`
-	DateFormat   bool              `json:"dateFormat"`
+	ID                     uint              `json:"id"`
+	Locale                 string            `json:"locale"`
+	ViewMode               users.ViewMode    `json:"viewMode"`
+	SingleClick            bool              `json:"singleClick"`
+	Perm                   users.Permissions `json:"perm"`
+	Commands               []string          `json:"commands"`
+	LockPassword           bool              `json:"lockPassword"`
+	HideDotfiles           bool              `json:"hideDotfiles"`
+	DateFormat             bool              `json:"dateFormat"`
+	EditorAutoSaveInterval int               `json:"editorAutoSaveInterval"`
 }
 
 type authToken struct {
@@ -179,15 +180,16 @@ var renewHandler = withUser(func(w http.ResponseWriter, r *http.Request, d *data
 func printToken(w http.ResponseWriter, _ *http.Request, d *data, user *users.User) (int, error) {
 	claims := &authToken{
 		User: userInfo{
-			ID:           user.ID,
-			Locale:       user.Locale,
-			ViewMode:     user.ViewMode,
-			SingleClick:  user.SingleClick,
-			Perm:         user.Perm,
-			LockPassword: user.LockPassword,
-			Commands:     user.Commands,
-			HideDotfiles: user.HideDotfiles,
-			DateFormat:   user.DateFormat,
+			ID:                     user.ID,
+			Locale:                 user.Locale,
+			ViewMode:               user.ViewMode,
+			SingleClick:            user.SingleClick,
+			Perm:                   user.Perm,
+			LockPassword:           user.LockPassword,
+			Commands:               user.Commands,
+			HideDotfiles:           user.HideDotfiles,
+			DateFormat:             user.DateFormat,
+			EditorAutoSaveInterval: user.EditorAutoSaveInterval,
 		},
 		RegisteredClaims: jwt.RegisteredClaims{
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
