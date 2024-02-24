6352e12c8ec21276c9c446d410b0dc211715ad55
niubility000 <76441520+niubility000@users.noreply.github.com>
2024-02-25 00:30:07 +0800

    fix: remain in the same position of list after rename or delete Update4 Files.vue

--- frontend/src/views/Files.vue
+++ frontend/src/views/Files.vue
@@ -72,11 +72,11 @@ export default {
     $route: function (to, from) {
       if (from.path.endsWith("/")) { 
         if (to.path.endsWith("/"))   {
-          window.sessionStorage.setItem('listFrozen', "false");
+          window.sessionStorage.setItem("listFrozen", "false");
           this.fetchData();
           return;
         } else {
-          window.sessionStorage.setItem('listFrozen', "true");
+          window.sessionStorage.setItem("listFrozen", "true");
           this.fetchData();
           return;        
         }
@@ -117,7 +117,7 @@ export default {
       this.$store.commit("closeHovers");
 
       // Set loading to true and reset the error.
-      if (window.sessionStorage.getItem('listFrozen') !=="true" && window.sessionStorage.getItem('modified') !=="true"){ 
+      if (window.sessionStorage.getItem("listFrozen") !=="true" && window.sessionStorage.getItem("modified") !=="true"){ 
         this.setLoading(true);
       }
       this.error = null;
