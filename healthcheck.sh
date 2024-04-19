46b1bd15ddb2e51115e4c208c9193bbd77b4edd9
Andrew Kennedy <andrew-kennedy@users.noreply.github.com>
2024-04-19 02:06:25 -0700

    Handle quotes in healthcheck.sh
    This file as is makes a value of `"localhost"` as the address in the .filebrowser.json file read in as `"localhost"` instead of `localhost`. These quotes break the curl command. Using `-r` with jq fixes this.

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PORT=${FB_PORT:-$(jq .port /.filebrowser.json)}
-ADDRESS=${FB_ADDRESS:-$(jq .address /.filebrowser.json)}
+PORT=${FB_PORT:-$(jq -r .port /.filebrowser.json)}
+ADDRESS=${FB_ADDRESS:-$(jq -r .address /.filebrowser.json)}
 ADDRESS=${ADDRESS:-localhost}
 curl -f http://$ADDRESS:$PORT/health || exit 1
