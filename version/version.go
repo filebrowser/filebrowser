6eec5c11ccf0afaf396c0146c5e407398bc3f97f
Andreas Deininger <andreas@deininger.net>
2024-09-15 21:49:47 +0200

    Fix typos

--- version/version.go
+++ version/version.go
@@ -3,6 +3,6 @@ package version
 var (
 	// Version is the current File Browser version.
 	Version = "(untracked)"
-	// CommitSHA is the commmit sha.
+	// CommitSHA is the commit sha.
 	CommitSHA = "(unknown)"
 )
