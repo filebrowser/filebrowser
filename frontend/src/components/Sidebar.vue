2b37e696c9bde4d0c453de236a3555d982346bbb
Angelfisch <82846251+Angelfisch@users.noreply.github.com>
2024-09-30 11:05:23 +0200

    fix: added whitespace before version (#3510)

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -101,7 +101,7 @@
           href="https://github.com/filebrowser/filebrowser"
           >File Browser</a
         >
-        <span> {{ version }}</span>
+        <span> {{ ' ' }} {{ version }}</span>
       </span>
       <span>
         <a @click="help">{{ $t("sidebar.help") }}</a>
