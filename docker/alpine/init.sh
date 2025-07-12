01c814cf98f81f2bcd622aea75e5b1efe3484940
outlook84 <96007761+outlook84@users.noreply.github.com>
2025-07-12 19:30:36 +0800

    feat: Improve Docker entrypoint and config handling

--- docker/alpine/init.sh
+++ docker/alpine/init.sh
@@ -7,4 +7,19 @@ if [ ! -f "/config/settings.json" ]; then
   cp -a /defaults/settings.json /config/settings.json
 fi
 
-exec "$@"
+# Deal with the case where user does not provide a config argument
+has_config_arg=0
+for arg in "$@"; do
+  case "$arg" in
+  --config|--config=*|-c|-c=*)
+    has_config_arg=1
+    break
+    ;;
+  esac
+done
+
+if [ "$has_config_arg" -eq 0 ]; then
+  set -- --config=/config/settings.json "$@"
+fi
+
+exec filebrowser "$@"
