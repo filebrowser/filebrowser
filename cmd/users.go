1d35c7d1b705147dc02244e99064551a72b34314
Henrique Dias <mail@hacdias.com>
2025-11-15 08:47:20 +0100

    fix: remove duplicated 'hide-defaults' flag (is 'hideDefaults')

--- cmd/users.go
+++ cmd/users.go
@@ -80,7 +80,6 @@ func addUserFlags(flags *pflag.FlagSet) {
 	flags.Bool("dateFormat", false, "use date format (true for absolute time, false for relative)")
 	flags.Bool("hideDotfiles", false, "hide dotfiles")
 	flags.String("aceEditorTheme", "", "ace editor's syntax highlighting theme for users")
-	flags.Bool("hide-dotfiles", false, "Hide dotfiles by default")
 }
 
 func getViewMode(flags *pflag.FlagSet) (users.ViewMode, error) {
@@ -136,7 +135,7 @@ func getUserDefaults(flags *pflag.FlagSet, defaults *settings.UserDefaults, all 
 			defaults.Sorting.By, err = getString(flags, flag.Name)
 		case "sorting.asc":
 			defaults.Sorting.Asc, err = getBool(flags, flag.Name)
-		case "hide-dotfiles":
+		case "hideDotfiles":
 			defaults.HideDotfiles, err = getBool(flags, flag.Name)
 		}
 		if err != nil {
